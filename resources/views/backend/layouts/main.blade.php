69f9eb28ae49236ae9f5f3a61d755b7ab765d165
Kuroyasha <Rickyandrean41@gmail.com>
2022-06-21 19:30:41 +0700

    memperbaikki laporan view

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -131,6 +131,16 @@
     {{-- Popper js --}}
     <script src="https://unpkg.com/@popperjs/core@2/dist/umd/popper.js"></script>
 
+    {{-- DATA TABLES --}}
+
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+
 </body>
 
 </html>
--- resources/views/backend/report/index.blade.php
+++ resources/views/backend/report/index.blade.php
@@ -24,7 +24,7 @@
                 <button type="submit" class="btn btn-danger">Cari</button>
             </form>
         </div>
-
+        <hr>
         <table id="example" class="table table-striped table-bordered display nowrap" style="100%">
             <thead>
                 <tr align="CENTER">
@@ -71,22 +71,14 @@
         </table>
     </div>
 
-    {{-- DATA TABLES --}}
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
-
     <script>
         $(document).ready(function() {
             $('#example').DataTable({
                 dom: 'Bfrtip',
                 buttons: [
-                    'copy', 'csv', 'excel', 'pdf', 'print'
+                    // 'copy', 'csv', 
+                    'excel', 'pdf', 'print'
                 ]
             });
         });
